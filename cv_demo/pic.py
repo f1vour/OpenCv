5dd27a8b4e192960e294d9591b485105262babb5
favour <favourchukwuemeka004@gmail.com>
2024-07-18 01:17:53 +0100

    Modification

--- cv_demo/pic.py
+++ cv_demo/pic.py
@@ -2,11 +2,11 @@ import cv2
 
 face_haar_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 image = cv2.imread('demo.jpg')
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # This are the colours in BGR and the weight
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # This is the colours in BGR and the weight
 # cv2.imshow("Gray", gray)
 # cv2.waitKey()
-
-# Classifier is a classification to tell the front of a human face. it detects the face. Cv is used to load the classifier
+# Classifier is a classification to tell the front of a human face. it detects
+# the face. Cv is used to load the classifier
 
 faces = face_haar_cascade.detectMultiScale(gray, 1.1, 4) # This is our classifier, it detects the faces
 
